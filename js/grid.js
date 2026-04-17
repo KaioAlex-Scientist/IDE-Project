0c78bbefb969613cecda32ff9f03849cc7d88bfc
Eduardo Pires <eduardobrittopires@gmail.com>
2026-04-16 21:09:03 -0300

    Cards Grid - color e size
    Só coisa basica do grid

--- js/grid.js
+++ js/grid.js
@@ -1,58 +1,88 @@
+// ================= LISTA DE JOGOS =================
+
 const jogos = [
   {
     nome: "PEAK",
     preco: "R$ 29,90",
-    img: "img/jogos/PEAK.png"
+    img: "img/jogos/PEAK.png",
+    promocao: false
   },
   {
     nome: "Gran Turismo 4",
     preco: "R$ 39,90",
-    img: "img/jogos/GranTurismo4.png"
+    precoOriginal: "R$ 79,90",
+    desconto: "-50%",
+    img: "img/jogos/GranTurismo4.png",
+    promocao: true
   },
   {
     nome: "Need For Speed Pro-Street",
     preco: "R$ 49,90",
-    img: "img/jogos/needforspeedprostreet.png"
+    img: "img/jogos/needforspeedprostreet.png",
+    promocao: false
   },
   {
     nome: "Need For Speed Underground 2",
     preco: "R$ 49,90",
-    img: "img/jogos/needforspeedunderground2.png"
+    precoOriginal: "R$ 99,90",
+    desconto: "-50%",
+    img: "img/jogos/needforspeedunderground2.png",
+    promocao: true
   },
   {
     nome: "BULLY: Scholarship Edition",
     preco: "R$ 69,90",
-    img: "img/jogos/bully.png"
+    img: "img/jogos/bully.png",
+    promocao: false
   },
   {
     nome: "Destiny",
     preco: "R$ 79,90",
-    img: "img/jogos/destiny.png"
-  },
-  {
-    nome: "Overwatch",
-    preco: "R$ 89,90",
-    img: "img/jogos/Overwatch.png"
-  },
-  {
-    nome: "Dragon Ball Z Budokai Tenkaichi 3",
-    preco: "R$ 39,90",
-    img: "img/jogos/dragonballbudokaitenkaichi3.png"
+    precoOriginal: "R$ 119,90",
+    desconto: "-33%",
+    img: "img/jogos/destiny.png",
+    promocao: true
   }
 ];
 
+// ================= RENDERIZAÇÃO =================
+
 const listaJogos = document.getElementById("lista-jogos");
 
-jogos.forEach(jogo => {
-  listaJogos.innerHTML += `
-    <div class="col-12 col-sm-6 col-lg-3">
-      <div class="card jogo-card h-100">
-        <img src="${jogo.img}" class="card-img-top">
-        <div class="card-body">
-          <h5>${jogo.nome}</h5>
-          <p>${jogo.preco}</p>
+function renderJogos() {
+  let html = "";
+
+  jogos.forEach(jogo => {
+
+    html += `
+      <div class="col-12 col-sm-6 col-lg-3">
+        <div class="card jogo-card h-100 position-relative">
+
+          ${jogo.promocao ? `
+            <span class="badge-desconto">${jogo.desconto} OFF</span>
+          ` : ""}
+
+          <img src="${jogo.img}" class="card-img-top">
+
+          <div class="card-body">
+            <h5>${jogo.nome}</h5>
+
+            ${jogo.promocao ? `
+              <p class="preco-final">${jogo.preco}</p>
+              <p class="preco-original">${jogo.precoOriginal}</p>
+            ` : `
+              <p class="preco-normal">${jogo.preco}</p>
+            `}
+          </div>
+
         </div>
       </div>
-    </div>
-  `;
-});
+    `;
+  });
+
+  listaJogos.innerHTML = html;
+}
+
+// ================= INICIALIZAÇÃO =================
+
+renderJogos();
--- js/script.js
+++ js/script.js
@@ -9,11 +9,6 @@ document.getElementById("promo-img").src =
   imagens[Math.floor(Math.random() * imagens.length)];
 
 
-// Popup fake
-function popup(tipo) {
-  alert(tipo + " (texto fictício)");
-}
-
 // Fiz essa funçãozinha para carregar/fetch os componentes de uma outra pasta, assim fica 
 // mais fácil implementarmos em conjunto, coloquem seus componentes aqui! :P   -Pedro
 async function carregarComponente(id, caminho) {
@@ -21,14 +16,18 @@ async function carregarComponente(id, caminho) {
   const html = await fetch(caminho).then(r => r.text());
   el.innerHTML = html;
 }
+
 async function carregarComponentes() {
   await carregarComponente("carousel-id", "components/carousel.html");
   await carregarComponente("navbar", "components/navbar.html");
   await carregarComponente("form-id", "components/form.html");
   await carregarComponente("footer-id", "components/footer.html");
   await carregarComponente("janelas-id", "components/janelas.html");
+  await carregarComponente("grid-id", "components/grid.html");
+  
   /* tive que transformar o js do carousel em função pois ele estava iniciando antes do html ser fetch - Perdo */
   iniciarCarousel();
 }
+
 carregarComponentes();
 
