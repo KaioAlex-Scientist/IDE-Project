9878baf43009cfee5c3b6c52d2a76d1367f20856
Kaio Alexsander Inácio da Silva <kaioprocontact@gmail.com>
2026-04-17 22:16:42 -0300

    navbar responsiva

--- js/navbar.js
+++ js/navbar.js
@@ -1,18 +1,36 @@
-// Trocar tema
 function trocarTema() {
   document.body.classList.toggle("tema-roxo");
   document.body.classList.toggle("tema-preto");
 
   const body = document.body;
   const btn = document.getElementById("botao-tema");
-  if (body.classList.contains("tema-preto")) {
-    btn.src = "img/navbar/darkTheme.png";
-  } else {
-    btn.src = "img/navbar/lightTheme.png";
+
+  if (btn) {
+    if (body.classList.contains("tema-preto")) {
+      btn.src = "img/navbar/darkTheme.png";
+    } else {
+      btn.src = "img/navbar/lightTheme.png";
+    }
+  }
+
+  const btnMobile = document.getElementById("botao-tema-mobile");
+
+  if (btnMobile) {
+    if (body.classList.contains("tema-preto")) {
+      btnMobile.src = "img/navbar/darkTheme.png";
+    } else {
+      btnMobile.src = "img/navbar/lightTheme.png";
+    }
   }
 }
 
+function trocarTemaMobile() {
+  trocarTema();
+}
+
 function girarLogo() {
-  const logo = document.querySelector('.logo_02');
-  logo.classList.toggle('girando');
+  const logo = document.querySelector(".logo_02");
+  if (logo) {
+    logo.classList.toggle("girando");
+  }
 }
